bb22e2559419d547732dd8f4ecf5062f2155939f
Barc1710 <ba.rodascu@unsm.edu.pe>
2026-09-10 09:54:21 -0500

    feat(seguimiento): implementar vista completa y encabezado de seguimiento de registros

---
+++ src/app/features/seguimiento/encabezado-seguimiento/encabezado-seguimiento.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encabezado-seguimiento',
+  standalone: true,
+  templateUrl: './encabezado-seguimiento.component.html',
+  styleUrl: './encabezado-seguimiento.component.css'
+})
+export class EncabezadoSeguimientoComponent {}
